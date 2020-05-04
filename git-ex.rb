36fcc46e212cbf6074112cdd8f868cc59bbca25a
yagince <straitwalk@gmail.com>
2020-05-04 23:43:16 +0900

    update v0.0.2

--- git-ex.rb
+++ git-ex.rb
@@ -1,13 +1,13 @@
 class GitEx < Formula
   desc "A Tui for Git written in Rust"
   homepage "https://github.com/yagince/git-ex-es"
-  version "0.0.1"
-  url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.1/git-ex-v0.0.1-x86_64-apple-darwin.tar.gz"
-  sha256 "b5d8e11a98b08776a0681ebf9f09c193c861e059dff66530f9335c25005bed3b"
+  version "0.0.2"
+  url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.2/git-ex-v0.0.2-x86_64-apple-darwin.tar.gz"
+  sha256 "95c5ffa11d7c9caa6e16b1d392cba32f7ed807ca91a6539da3af76020f36739e"
 
   if OS.linux?
-    url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.1/git-ex-v0.0.1-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "02d21e5d7fd00472972f9949a01f7f345ccce30ddc3af0cce2cc2d0b29407940"
+    url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.2/git-ex-v0.0.2-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "960dd299fe4732569b91f4e7206e0b0e6b5f2df10be9946e0dcf351c8210388a"
   end
 
   def install
---
+++ update.rb
@@ -0,0 +1,30 @@
+require 'openssl'
+require 'open-uri'
+
+version = "v0.0.2"
+targets = ["x86_64-apple-darwin", "x86_64-unknown-linux-musl"]
+
+digests = targets.map{|x|
+  [x, OpenSSL::Digest::SHA256.hexdigest(URI.parse("https://github.com/yagince/git-ex-rs/releases/download/#{version}/git-ex-#{version}-#{x}.tar.gz").read)]
+}.to_h
+
+TEMPLATE = <<EOS
+class GitEx < Formula
+  desc "A Tui for Git written in Rust"
+  homepage "https://github.com/yagince/git-ex-es"
+  version "#{version[1..]}"
+  url "https://github.com/yagince/git-ex-rs/releases/download/#{version}/git-ex-#{version}-x86_64-apple-darwin.tar.gz"
+  sha256 "#{digests[targets[0]]}"
+
+  if OS.linux?
+    url "https://github.com/yagince/git-ex-rs/releases/download/#{version}/git-ex-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "#{digests[targets[1]]}"
+  end
+
+  def install
+    bin.install "git-ex"
+  end
+end
+EOS
+
+open("git-ex.rb", "w"){ |f| f.puts TEMPLATE }
