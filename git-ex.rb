7569226a7003b5410f22c1a854f124efa83e57f8
yagince <straitwalk@gmail.com>
2020-05-06 23:31:14 +0900

    release v0.1.1

--- git-ex.rb
+++ git-ex.rb
@@ -1,13 +1,13 @@
 class GitEx < Formula
   desc "A Tui for Git written in Rust"
   homepage "https://github.com/yagince/git-ex-es"
-  version "0.0.2"
-  url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.2/git-ex-v0.0.2-x86_64-apple-darwin.tar.gz"
-  sha256 "95c5ffa11d7c9caa6e16b1d392cba32f7ed807ca91a6539da3af76020f36739e"
+  version "0.1.1"
+  url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.1/git-ex-v0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "6dfe11fc67e4edba88bc1feb6f4ea0609e51efdba6eb76d212a25294a136473c"
 
   if OS.linux?
-    url "https://github.com/yagince/git-ex-rs/releases/download/v0.0.2/git-ex-v0.0.2-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "960dd299fe4732569b91f4e7206e0b0e6b5f2df10be9946e0dcf351c8210388a"
+    url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.1/git-ex-v0.1.1-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "9c9454843e776755e831000e8918619138943d29d2d5671f22942798c072ae3d"
   end
 
   def install
--- update.rb
+++ update.rb
@@ -1,7 +1,7 @@
 require 'openssl'
 require 'open-uri'
 
-version = "v0.0.2"
+version = "v0.1.1"
 targets = ["x86_64-apple-darwin", "x86_64-unknown-linux-musl"]
 
 digests = targets.map{|x|
