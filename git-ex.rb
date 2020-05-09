20ff8c97edbcf898739c0db68d011ab71b76cc62
yagince <straitwalk@gmail.com>
2020-05-09 22:47:41 +0900

    release v0.1.3

--- git-ex.rb
+++ git-ex.rb
@@ -1,13 +1,13 @@
 class GitEx < Formula
   desc "A Tui for Git written in Rust"
   homepage "https://github.com/yagince/git-ex-es"
-  version "0.1.2"
-  url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.2/git-ex-v0.1.2-x86_64-apple-darwin.tar.gz"
-  sha256 "76ec861b2abed49a908d830fecbdd387b974d92fd39bb2ea936ec260d57d02c1"
+  version "0.1.3"
+  url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.3/git-ex-v0.1.3-x86_64-apple-darwin.tar.gz"
+  sha256 "e6d60d354a1340b28d8fcf2dcee9bff4cc000512d7b608858794fcc2bee9ab6a"
 
   if OS.linux?
-    url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.2/git-ex-v0.1.2-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "3b184f72a5a34b39ed02e11b82a8c9cce82d94b089647dd6cf75710f7336a0a1"
+    url "https://github.com/yagince/git-ex-rs/releases/download/v0.1.3/git-ex-v0.1.3-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "7bc3c51091a4e8747cca16e9b8ddf7c54cdc497c1bb4704f61d2a38bccf9c90f"
   end
 
   def install
